01f853d1818a1d348eb7bfd5d954438ebdfd7826
Platypudding <wittoakes@gmail.com>
2019-06-18 04:46:15 -0500

    Add files via upload

---
+++ Marwynnfinite.py
@@ -0,0 +1,23 @@
+#Input power as Marwynn's power at the very beginning of the loop
+Power = 1
+
+#Number of times you go through the full tap and untap process
+Untap = 10
+
+#Amount of mana in the pool. Always starts at 0
+ManaPool = 0
+
+#This loop basically simulates the process of tapping Marwynn for her power value, and untapping her for 3 mana, leaving her in an untapped state
+for x in range(Untap):  
+    ManaPool = ManaPool + Power - 3
+    Power = Power + 2
+    
+    #Below ensures that you don't get negative values for the amount of mana in pool, which can't go below 0 and offputs the ManaPool output by 2 if it does
+    if ManaPool < 0:
+        ManaPool = 0
+    
+    if x == Untap:
+        break
+    
+print(Power)
+print(ManaPool)
